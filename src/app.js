0da99cc7167da3bf9b8f2db46f9cc52d6ce5e181
jzpoe <pmguez1287@gmail.com>
2025-07-16 14:46:17 -0500

    configuracion de cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,23 @@ config();
 const app = express();
 
 app.use(cors());
-app.use(json());
+const allowedOrigins = [
+  'https://it-inventario.vercel.app', // Tu frontend en producción
+  'http://localhost:3000'             // Opcional para desarrollo local
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Permitir peticiones sin origin (como en Postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 // Conexión a la base de datos
 dbConnect()
 
